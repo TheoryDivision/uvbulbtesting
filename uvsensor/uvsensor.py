c8d23b228838e980dfd1408459dd1714a199f3e7
Vishhvaan Gopalakrishnan <vish0288@gmail.com>
2021-02-22 10:21:10 -0500

    Update requirements

--- uvsensor/uvsensor.py
+++ uvsensor/uvsensor.py
@@ -2,10 +2,8 @@ import board
 import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-import csv
 import datetime
 import asyncio
-import time
 
 class UVsensor:
     def __init__(self, pin):
