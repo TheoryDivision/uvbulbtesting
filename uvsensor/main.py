89f3b528e91d07e14101610a92ce23ad33494f31
Vishhvaan Gopalakrishnan <vish0288@gmail.com>
2021-02-23 21:59:00 -0500

    async uvbot v1.2 add more bot responses

--- uvsensor/main.py
+++ uvsensor/main.py
@@ -10,8 +10,8 @@ from uvbot import uvbot
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='UV Degredation Slack Bot')
     parser.add_argument('-p', '--pin', default='0', type=int, help='Pin of ADC sensor is attached to')
-    parser.add_argument('-s', '--sint', default='15', type=float, help='Interval for sensing')
-    parser.add_argument('-g', '--gint', default='90', type=float, help='Interval for graphing')
+    parser.add_argument('-s', '--sint', default='15', type=float, help='Interval (minutes) for sensing')
+    parser.add_argument('-g', '--gint', default='90', type=float, help='Interval (minutes) for graphing')
     parser.add_argument('-o', '--output', default='uvdata.csv', type=str, help='File name for data')
     parser.add_argument('-i', '--image', default='graph.png', type=str, help='Image name for graph')
 
--- uvsensor/uvbot.py
+++ uvsensor/uvbot.py
@@ -1,10 +1,11 @@
 from slackeventsapi import SlackEventAdapter
 import yaml
+import re
 
 from degtester import DegTester
 
 def uvbot(tester, fpath, ipath):
-    greetings = ["hi", "hello", "hey", "hai"]
+    greetings = ["hi", "hello", "hey", "hai", "sup"]
     data_req = ["data", "csv"]
     graph_req = ["graph", "plot"]
 
@@ -13,23 +14,43 @@ def uvbot(tester, fpath, ipath):
 
     slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events")
 
+    def extract_mins(string):
+        numbers = re.findall(r"[-+]?\d*\.?\d+|[-+]?\d+", string)
+        if "min" in string:
+            return float(numbers[-1])
+        if "hour" in string:
+            return float(numbers[-1]*60)
+
     @slack_events_adapter.on("app_mention")
     def handle_message(event_data):
             message = event_data["event"]
+            mtext = message.get('text').lower()
             if message.get("subtype") is None:
                 channel = message["channel"]
-                if any(txt in message.get('text').lower() for txt in greetings):
+                if any(txt in mtext for txt in greetings):
                     tester.send_message(channel, f'Hello <@{message["user"]}>! :party_parrot:')
-                elif "uptime" in message.get('text').lower():
+                elif "uptime" in mtext:
                     days = round(tester.sensor.uptime(),2)
                     tester.send_message(channel, f"{days} days")
-                elif any(txt in message.get('text').lower() for txt in data_req):
+                elif any(txt in mtext for txt in data_req):
                     tester.upload_file(channel, "Here is the most recent file:", fpath)
-                elif any(txt in message.get('text').lower() for txt in graph_req):
+                elif "param" in mtext:
+                    tester.send_message(channel, f"Read/Write Interval: {tester.sint} minutes\nGraphing Interval: {tester.gint} minutes")
+                elif "adjust" in mtext:
+                    if "rw" in mtext:
+                        tester.sint = extract_mins(mtext)
+                        tester.send_message(channel, "Confirmed.")
+                        tester.send_message(channel, f" Changed Read/Write Interval: {tester.sint} minutes\nGraphing Interval: {tester.gint} minutes")
+                    elif "graph" in mtext:
+                        tester.gint = extract_mins(mtext)
+                        tester.send_message(channel, "Confirmed.")
+                        tester.send_message(channel, f"Read/Write Interval: {tester.sint} minutes\nChanged Graphing Interval: {tester.gint} minutes")
+                elif any(txt in mtext for txt in graph_req):
                     if tester.grapher.gs:
                         tester.upload_file(channel, "Here you go:", ipath)
                     else:
                         tester.send_message(channel, "A graph has not been generated yet. Check back later.")
+
 
     @slack_events_adapter.on("error")
     def error_handler(err):
